fae42e07ad8fa69c0de43f5f7574557efa309196
Arpan Maheshwari <maheshwariarpan828@gmail.com>
2022-02-27 12:20:14 +0530

    First Commit

---
+++ src/MyComponents/About.js
@@ -0,0 +1,68 @@
+import React from "react";
+
+export const About = () => {
+  return (
+    <div>
+      <p className="my-2 mx-3">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore
+        aliquid adipisci pariatur esse natus omnis repudiandae mollitia ratione,
+        nemo nulla! At ea, error doloribus ex iusto veniam aspernatur adipisci
+        dolores dolorum nihil, ipsa tempore dolore minus similique ut harum esse
+        aut quae eveniet repudiandae accusamus? Amet a quae iure laboriosam
+        aspernatur accusamus eos, ipsa unde minima quis nihil voluptatem,
+        praesentium explicabo. Veritatis, accusantium ex! Iste expedita
+        laudantium deleniti adipisci culpa accusamus quod. Ratione quae eligendi
+        error tempora labore officia ipsa repellat magni impedit obcaecati
+        ducimus animi omnis mollitia quia, earum corporis, voluptatem libero est
+        nesciunt aut. Quisquam impedit animi sunt possimus dolorem accusamus,
+        vitae ipsum quas hic amet? Amet repellat pariatur quasi eaque id nobis
+        natus repellendus, officiis tenetur? Sed, provident. Quia aliquid quos
+        dicta, repudiandae porro quaerat, ipsam quisquam aut ducimus eum tenetur
+        officia. Nesciunt iusto temporibus, adipisci repudiandae hic dolores
+        omnis eum a, iste quis vel, corporis aliquid soluta explicabo magni
+        ipsam deleniti natus cum similique aspernatur delectus exercitationem.
+        Quisquam iste unde cupiditate odio officiis velit earum numquam cum
+        officia magnam natus, adipisci maiores ea commodi id dolorem quasi
+        possimus dignissimos pariatur necessitatibus minima aut tempore? Nisi
+        adipisci enim ut qui perferendis omnis repellat itaque molestiae odio
+        tempore. Quia sequi labore ut repellendus. Commodi id deserunt unde esse
+        eligendi dolores minima culpa, laborum repellendus, quaerat voluptatibus
+        voluptates aspernatur similique excepturi, dolorum possimus nobis ad
+        tenetur aut enim doloremque laboriosam nemo dolorem pariatur! Commodi
+        tempore tempora totam voluptates qui fuga non quisquam reprehenderit
+        sint, architecto possimus corrupti quas at aspernatur enim fugit!
+        Quaerat rem totam harum molestiae illum explicabo nulla cupiditate earum
+        quisquam ratione inventore exercitationem eius accusantium obcaecati sed
+        enim dolor vel, similique voluptates ipsa est cumque? Voluptatum
+        temporibus odio tenetur molestias excepturi ipsam nam quibusdam in atque
+        fugiat, quasi vero dolor quae repellat asperiores distinctio minima!
+        Autem praesentium eum recusandae nemo tempora consequuntur sapiente
+        amet, temporibus vel voluptas exercitationem eligendi earum iusto
+        inventore, ex vero sunt voluptate. Vitae laboriosam quia corrupti. Est
+        itaque nemo dolorem, molestias voluptas soluta repellendus sunt sint
+        nulla. Repellendus odio officiis non quasi sunt, omnis eius asperiores.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur
+        excepturi distinctio, adipisci saepe minima dolorem molestiae ratione,
+        veniam soluta repellendus officia harum ex consequuntur sequi similique
+        impedit facilis molestias sit inventore atque consequatur reprehenderit.
+        Omnis repellendus molestias voluptatem at totam in corporis vitae error
+        pariatur, eligendi ab nisi quidem quae accusamus doloremque vero.
+        Distinctio explicabo deleniti modi quis, in voluptates nisi tempora?
+        Placeat quisquam cum tenetur ratione autem voluptatum quos laborum,
+        ipsam, dolores perferendis rem aut ipsum quia molestias optio ea velit
+        atque cumque sed? Eligendi ab quasi distinctio explicabo error molestiae
+        necessitatibus veritatis architecto a optio harum, atque quod? Lorem
+        ipsum dolor sit amet consectetur adipisicing elit. Nam, est! Laudantium
+        numquam distinctio, officiis eaque beatae dicta fugiat labore
+        repudiandae dolorem quos incidunt aut a, ipsum molestias modi provident
+        possimus minus? Quibusdam dolorem repellendus, consequuntur placeat,
+        distinctio ab nisi tempore aperiam architecto fuga molestias nihil earum
+        possimus vitae, itaque neque. Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Consequatur, dolorem! Lorem ipsum dolor sit amet.
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea, impedit.
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odio,
+        voluptatibus.
+      </p>
+    </div>
+  );
+};
